1ed1499051e14ef60b1fa0f21621014a70417c9d
TommyDo <dhhuy0704@gmail.com>
2025-10-19 15:10:00 -0600

    Add Vehicle model

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    protected $fillable = [
+        'type',
+        'name',
+        'manufacturer',
+        'model',
+        'year',
+        'fuel_capacity',
+        'status',
+        'note',
+    ];
+
+    const TYPES = [
+        'car' => 'Car',
+        'truck' => 'Truck',
+        'van' => 'Van',
+        'camper' => 'Camper',
+    ];
+
+    const STATUSES = [
+        'active' => 'Active',
+        'broken' => 'Broken',
+        'sold' => 'Sold',
+    ];
+}
